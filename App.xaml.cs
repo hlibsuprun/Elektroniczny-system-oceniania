ac7132d531be7e8679b30810736487c00ad93496
Hlib Suprun <hlibsuprun@gmail.com>
2024-02-01 03:24:33 +0100

    add auth window

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,11 +4,7 @@ using System.Windows;
 
 namespace Elektroniczny_system_oceniania
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
-
 }
