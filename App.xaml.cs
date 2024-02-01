3154be6e3ce4000901c15a894e3aa89c10666637
Hlib Suprun <hlibsuprun@gmail.com>
2024-02-01 02:59:02 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Elektroniczny_system_oceniania
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
